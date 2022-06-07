03b1181099c2a209d404a07a6ed89f0d48b4bab2
Onur Omer SAHIN <onuromersahin@gmail.com>
2022-06-07 21:11:54 +0300

    CategoryController and category twig pages created

---
+++ src/Controller/Admin/CategoryController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/admin/category/list", name="admin_category_list")
+     */
+    public function listCagetories()
+    {
+        return $this->render('admin/category/list.html.twig', [
+            'controller_name' => 'CategoryController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/category/add", name="admin_category_add")
+     */
+    public function addCagetories()
+    {
+        return $this->render('admin/category/add.html.twig', [
+            'controller_name' => 'CategoryController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/category/edit", name="admin_category_edit")
+     */
+    public function editCagetories()
+    {
+        return $this->render('admin/category/edit.html.twig', [
+            'controller_name' => 'CategoryController',
+        ]);
+    }
+}
